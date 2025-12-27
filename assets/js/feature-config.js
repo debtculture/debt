ca7233087508ad46fc24b7370413378ad17c608f
Autopsy <scottielongypr@gmail.com>
2025-12-27 11:52:37 -0600

    Update feature-config.js

--- assets/js/feature-config.js
+++ assets/js/feature-config.js
@@ -1,4 +1,5 @@
 const FEATURES = {
     walletConnect: false,  // Set to true to enable wallet features
-    forums: false          // Set to true to show forums tab
+    forums: false,         // Set to true to show forums tab
+    games: false           // Set to true to show games tab
 };
